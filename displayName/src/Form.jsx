34a3acd129ead9f6c54f335d715a01c1aa5fafc3
pritamkumarbishwas <pritamkumarbishwas85@gmail.com>
2024-03-02 23:31:21 +0530

    fix log issue

--- displayName/src/Form.jsx
+++ displayName/src/Form.jsx
@@ -1,22 +1,23 @@
-import { useEffect, useState } from 'react'
-import './Form.css'
+import { useEffect, useState } from 'react';
+import './Form.css';
 
 function Form() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [submitted, setSubmitted] = useState(false);
 
-  const handelFirstNameChange = (event) => {
+  const handleFirstNameChange = (event) => {
     setFirstName(event.target.value);
-  }
-  const handelLastNameChange = (event) => {
-    setLastName(event.target.value);
-  }
+  };
 
-  const handelFormSubmit = (event) => {
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
     event.preventDefault();
     setSubmitted(true);
-  }
+  };
 
   useEffect(() => {
     if (firstName !== '' && lastName !== '') {
@@ -24,41 +25,32 @@ function Form() {
     }
   }, [firstName, lastName]);
 
-
   return (
-    <>
-      <div className='container'>
-        <div className='card'>
-          <form onSubmit={handelFormSubmit}>
-            <div className='col-sm-6'>
-              {submitted && (firstName !== '' && lastName !== '') ? (
-                <h3> full name: {firstName} {lastName}</h3>
-              ) : (
-                <h3>full name:</h3>
-              )}
+    <div className='container'>
+      <div className='card'>
+        <form onSubmit={handleFormSubmit}>
+          <div className='col-sm-6'>
+            {/* Simplified conditional rendering for the full name */}
+            <h3>Full Name: {submitted ? `${firstName} ${lastName}` : ''}</h3>
+          </div>
 
+          <div className='col-sm-6'>
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" id="firstName" value={firstName} onChange={handleFirstNameChange} required />
+          </div>
 
-            </div>
+          <div className='col-sm-6'>
+            <label htmlFor='lastName'>Last Name</label>
+            <input type='text' id='lastName' value={lastName} onChange={handleLastNameChange} required />
+          </div>
 
-            <div className='col-sm-6'>
-              <label htmlFor="First Name">First Name</label>
-              <input type="text" onChange={handelFirstNameChange} required />
-            </div>
-
-            <div className='col-sm-6'>
-              <label htmlFor='Last Name'>Last Name</label>
-              <input type='text' onChange={handelLastNameChange} required />
-            </div>
-
-            <div className='col-sm-6'>
-              <button type='submit'>Submit</button>
-            </div>
-
-          </form>
-        </div>
+          <div className='col-sm-6'>
+            <button type='submit'>Submit</button>
+          </div>
+        </form>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default Form
+export default Form;
