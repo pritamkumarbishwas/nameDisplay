8c892f1fc582e435d72e1a8d87ea0d39192f8611
pritamkumarbishwas <pritamkumarbishwas85@gmail.com>
2024-03-02 23:15:58 +0530

    fix log issue

--- displayName/src/Form.jsx
+++ displayName/src/Form.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './Form.css'
 
 function Form() {
@@ -15,7 +15,6 @@ function Form() {
 
   const handelFormSubmit = (event) => {
     event.preventDefault();
-    console.log(firstName, lastName);
     setSubmitted(true);
   }
 
@@ -25,16 +24,22 @@ function Form() {
         <div className='card'>
           <form onSubmit={handelFormSubmit}>
             <div className='col-sm-6'>
-              <h3> Full Name: {submitted && `${firstName} ${lastName}`}</h3>
+              {submitted || (firstName !== '' && lastName !== '') ? (
+                <h3> Full Name: {firstName} {lastName}</h3>
+              ) : (
+                <h3>Full Name:</h3>
+              )}
+
+
             </div>
 
             <div className='col-sm-6'>
-              <label htmlFor="first name">First Name</label>
+              <label htmlFor="First Name">First Name</label>
               <input type="text" onChange={handelFirstNameChange} required />
             </div>
 
             <div className='col-sm-6'>
-              <label htmlFor='last name'>Last Name</label>
+              <label htmlFor='Last Name'>Last Name</label>
               <input type='text' onChange={handelLastNameChange} required />
             </div>
 
